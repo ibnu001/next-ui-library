f90a8d543b34befa8371967796cf5df6e12c4a5e
frontend <frontend@mykonsul.com>
2024-08-19 16:03:43 +0700

    fix: change color button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,7 +7,7 @@ const Button = ({title, btnType, containerStyles, handleClick}: ButtonProps) => 
     return (
         <button
             type={btnType || "button"}
-            className={containerStyles + ' text-white bg-blue-600 max-mobile:bg-blue-950 max-tablet:bg-blue-300 ' +
+            className={'text-yellow-300 bg-blue-600 max-mobile:bg-blue-950 max-tablet:bg-blue-300 ' +
                 'max-desktop:bg-red-600 max-desktopLg:bg-red-950 rounded-full min-w-[130px] ' +
                 'py-2 px-4 hover:bg-blue-950 font-sans'}
             onClick={handleClick}
