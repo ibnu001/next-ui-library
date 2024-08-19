d6919e8cfcc8489e2d193488d8e6514f4cb8b8fe
frontend <frontend@mykonsul.com>
2024-08-19 15:43:14 +0700

    add: tailwind-preset.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-const config: Config = {
+export const baseConfig: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -26,4 +26,9 @@ const config: Config = {
   },
   plugins: [],
 };
+
+const config: Config = {
+  ...baseConfig,
+};
+
 export default config;
